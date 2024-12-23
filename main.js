a33442a720bd151feb352f818ee5bea3602c0c4c
piskevet <137320739+piskevet@users.noreply.github.com>
2024-12-23 22:16:41 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,629 @@
+let quizData = [
+    {
+      question: "Batu Nereli?",
+      options: ["Kırıkkale", "Ankara", "İstanbul", "Ordu"],
+      correct: "Kırıkkale",
+    },
+    {
+      question: "Batu Hangi Gün Doğdu?",
+      options: ["27", "23", "11", "26"],
+      correct: "27",
+    },
+    {
+      question:
+        "Batu'nun Favori Mentalist Karakteri Hangisi?",
+      options: [
+        "Patrick Jane",
+        "Teresa Lisbon",
+        "Red John",
+        "Grace Van Pelt",
+      ],
+      correct: "Patrick Jane",
+    },
+    {
+      question: "Batu Hangi Ay Doğdu?",
+      options: ["Ağustos", "Ne bilim amk", "Ocak", "Şubat"],
+      correct: "Şubat",
+    },
+    {
+      question: "Batu Hangi Yıl Doğdu?",
+      options: [
+        "2008",
+        "2009",
+        "2010",
+        "1988",
+      ],
+      correct: "2009",
+    },
+    {
+      question: "Batu'nun En Sevdiği Yemek?",
+      options: [
+        "Tavuk Pilav",
+        "Pizza",
+        "Tavuk Döner",
+        "Menemen",
+      ],
+      correct: "Tavuk Pilav",
+    },
+    {
+      question: "Batu Hangi Okulda Okuyor(Lise)?",
+      options: [
+        "Çapa Fen",
+        "Pertevniyel",
+        "Kabataş Erkek",
+        "Vefa",
+      ],
+      correct: "Vefa",
+    },
+    {
+      question:
+        "Puskivet hesabında 23.12.2024 tarihinde kaç post vardır?",
+      options: [
+        "3",
+        "4",
+        "5",
+        "6",
+      ],
+      correct: "5",
+    },
+    {
+      question: "Batu'nun Goat'ı kimdir?",
+      options: [
+        "Cristiano Ronaldo",
+        "Lebron James",
+        "Lionel Messi",
+        "Kobe Bryant",
+      ],
+      correct: "Lionel Messi",
+    },
+    {
+      question: "Puskivet Hesabının Takipçi Sayısı Kaçtır?",
+      options: [
+        "20",
+        "21",
+        "22",
+        "23",
+      ],
+      correct: "21",
+    },
+    {
+      question: "Batu'nun En Sevdiği Evcilleşebilen Hayvan?",
+      options: [
+        "Kedi",
+        "Köpek",
+        "Kaplumbağa",
+        "Salyangoz",
+      ],
+      correct: "Köpek",
+    },
+    {
+      question: "Batu Küçükken Hangi Takımlıydı?",
+      options: [
+        "Fenerbahçe",
+        "Galatasaray",
+        "Beşiktaş",
+        "Başakşehir",
+      ],
+      correct: "Fenerbahçe",
+    },
+    {
+      question: "Batu'nun Peak Rank Hangisidir?",
+      options: [
+        "İmmortal",
+        "Diamond",
+        "Radiant",
+        "Ascendant",
+      ],
+      correct: "İmmortal",
+    },
+    {
+      question: "En Sevdiği Renk?",
+      options: [
+        "Mavi",
+        "Tuana",
+        "Yeşil",
+        "Sarı",
+      ],
+      correct: "Yeşil",
+    },
+    {
+      question: "Batu'nun Hayalindeki Meslek?",
+      options: [
+        "Yazılım Mühendisi",
+        "Bilgisayar Mühendisi",
+        "Matematik Öğretmeni",
+        "İnşaatta Amele",
+      ],
+      correct: "Yazılım Mühendisi",
+    },
+    {
+      question: "Batu'da Hangi Hastalık Vardır?",
+      options: [
+        "Astım",
+        "Akciğer Kanseri",
+        "Şizofreni",
+        "Yok",
+      ],
+      correct: "Astım",
+    },
+    {
+      question: "En Sevdiği Tatlı?",
+      options: [
+        "Sufle",
+        "Brownie",
+        "Magnolya",
+        "Puding",
+      ],
+      correct: "Magnolya",
+    },
+    {
+      question: "Neye Alerjisi Var?",
+      options: [
+        "Toz",
+        "Sivrisinek",
+        "Fındık",
+        "Yok",
+      ],
+      correct: "Yok",
+    },
+    {
+      question: "Batu Hangisini Yapamıyor?",
+      options: [
+        "Smaç",
+        "Zıplayarak Servis",
+        "Nokta Pas",
+        "Manşet",
+      ],
+      correct: "Zıplayarak Servis",
+    },
+    {
+      question: "Batu'nun En Düşük Sınav Notu Nedir?",
+      options: [
+        "37",
+        "46",
+        "53",
+        "71",
+      ],
+      correct: "37",
+    },
+    {
+      question: "En Sevdiği Rapçi?",
+      options: [
+        "Şehinşah",
+        "Allame",
+        "Hidra",
+        "Anıl Piyancı",
+      ],
+      correct: "Allame",
+    },
+    {
+      question: "En Sevdiği Freestyle Atan Rapçi?",
+      options: [
+        "Hidra",
+        "Allame",
+        "Şehinşah",
+        "Grogi",
+      ],
+      correct: "Hidra",
+    },
+    {
+      question: "Hangi Tarihte Tuana ile 1. Yılımız Olucak?",
+      options: [
+        "16 Ağustos 2025",
+        "16 Haziran 2025",
+        "16 Temmuz  2025",
+        "16 Eylül 2025",
+      ],
+      correct: "16 Ağustos 2025",
+    },
+    {
+      question: "Tuana Batu'nun Telefonunda Nasıl Kayıtlıdır?",
+      options: [
+        "Tuanawoldee",
+        "Tuanawoldeee",
+        "Anası Arı",
+        "Bebiş",
+      ],
+      correct: "Anası Arı",
+    },
+    {
+      question: "Batu'nun en Sevdiği Mevsim?",
+      options: [
+        "İlkbahar",
+        "Yaz",
+        "Sonbahar",
+        "Kış",
+      ],
+      correct: "Kış",
+    },
+    {
+      question: "Batu'nun Telefon Numarası'nın Son 4 Hanesi?",
+      options: [
+        "0671",
+        "5148",
+        "0716",
+        "9175",
+      ],
+      correct: "0671",
+    },
+    {
+      question: "Batu'nun Babasının Adı?",
+      options: [
+        "Ali",
+        "Mehmet",
+        "Kudret",
+        "Baba",
+      ],
+      correct: "Kudret",
+    },
+    {
+      question: "Batu'nun Annesinin Adı?",
+      options: [
+        "Emine",
+        "Sıla",
+        "Beril",
+        "Anne",
+      ],
+      correct: "Emine",
+    },
+    {
+      question: "Batu'nun Abisinin Adı?",
+      options: [
+        "Batuhan",
+        "Abi",
+        "Serkan",
+        "Yok",
+      ],
+      correct: "Serkan",
+    },
+    {
+      question: "Batu'nun Ablasının Adı?",
+      options: [
+        "Sıla",
+        "Ecem",
+        "İlayda",
+        "Yok",
+      ],
+      correct: "Yok",
+    },
+    {
+      question: "Batu'nun En Sevdiği Oyun?",
+      options: [
+        "Red Dead Redemption 2",
+        "Minecraft",
+        "Gta 5",
+        "God Of War",
+      ],
+      correct: "Red Dead Redemption 2",
+    },
+    {
+      question: "Batu'nun En Nefret Ettiği Oyun?",
+      options: [
+        "Roblox",
+        "League Of Legends",
+        "Valorant",
+        "CS",
+      ],
+      correct: "League Of Legends",
+    },
+    {
+      question: "Batu'nun Discord'da Avatar'ında Hangi Efekt Vardır?",
+      options: [
+        "Kedi",
+        "Korsan",
+        "Sünger Bob",
+        "Valorant",
+      ],
+      correct: "Kedi",
+    },
+    {
+      question: "Batu'nun Okula Giderken Kullandığı Otobüs?",
+      options: [
+        "146B",
+        "78",
+        "97T",
+        "76D",
+      ],
+      correct: "78",
+    },
+    {
+      question: "Batu'nun Hayalindeki Üniversite?",
+      options: [
+        "İÜ",
+        "ODTÜ",
+        "Boğaziçi",
+        "Ne Bilim",
+      ],
+      correct: "Ne Bilim",
+    },
+    {
+      question: "Batu Aşağıdakilerden Hangisini Şuana Kadar Yapmamıştır?",
+      options: [
+        "Uyuya Kalmak",
+        "Mesaja Bakmamak",
+        "Tokayı Çıkartmak",
+        "Aldatmak",
+      ],
+      correct: "Aldatmak",
+    },
+    {
+      question: "Puana Mı İhtiyacın Var Sadece Bir Doğru?",
+      options: [
+        "Bir",
+        "Dokuz",
+        "Sıfır",
+        "Yedi",
+      ],
+      correct: "Bir",
+    },
+    {
+      question: "Fenerdeki Favori Oyuncum?",
+      options: [
+        "Amrabat",
+        "Dzeko",
+        "Tadic",
+        "Oosterwolde",
+      ],
+      correct: "Oosterwolde",
+    },
+    {
+      question: "Batu'ya Göre En İyi Kız Tipi?",
+      options: [
+        "Sarışın",
+        "Esmer",
+        "Kumral",
+        "Standartı mı var bu lavuğun",
+      ],
+      correct: "Standartı mı var bu lavuğun",
+    },
+    {
+      question: "Favori Spor Dalı?",
+      options: [
+        "Futbol",
+        "Voleybol",
+        "Basketbol",
+        "Masa Tenisi",
+      ],
+      correct: "Voleybol",
+    },
+    {
+      question: "Batu'nun Abisi 26 Yaşındadır",
+      options: [
+        "Doğru",
+        "Yanlış",
+      ],
+      correct: "Yanlış",
+    },
+    {
+      question: "Batu'nun Göz Rengi Kahverengidir",
+      options: [
+        "Doğru",
+        "Yanlış",
+      ],
+      correct: "Doğru",
+    },
+    {
+      question: "Batu'nun Saçı Siyahtır",
+      options: [
+        "Doğru",
+        "Yanlış",
+      ],
+      correct: "Doğru",
+    },
+    {
+      question: "Batu ile ... aydır çıkıyoruz.",
+      options: [
+        "2",
+        "3",
+        "4",
+        "5",
+      ],
+      correct: "4",
+    },
+    {
+      question: "Batuhan trip yerken hep ... der",
+      options: [
+        "...",
+        "cnm",
+        "naptımkine",
+        "3. kişilik ile konuşabilir miyim?",
+      ],
+      correct: "...",
+    },
+    {
+      question: "İremle Batu Sınıf Arkadaşıdır",
+      options: [
+        "Doğru",
+        "Yanlış",
+      ],
+      correct: "Yanlış",
+    },
+    {
+      question: "Ilgımla Batu Sınıf Arkadaşıdır",
+      options: [
+        "Doğru",
+        "Yanlış",
+      ],
+      correct: "Doğru",
+    },
+    {
+      question: "Arjenle Batu Sıra Arkadaşıdır",
+      options: [
+        "Doğru",
+        "Yanlış",
+      ],
+      correct: "Yanlış",
+    },
+    {
+      question: "Eceyle Batu Sınıf Arkadaşıdır",
+      options: [
+        "Doğru",
+        "Yanlış",
+      ],
+      correct: "Doğru",
+    },
+    {
+      question: "Zeynep Elif Gönen ...'dır",
+      options: [
+        "Kültür Sanat Başkanı",
+        "Kültür Sanat Tasarım Başkanı",
+        "Kültür Sanat Sponsor Başkanı",
+        "Kültür Sanat Başkan Yardımcısı",
+      ],
+      correct: "Kültür Sanat Başkanı",
+    },
+  ];
+  
+  const quizContainer = document.querySelector(".quiz-container");
+const question = document.querySelector(".quiz-container .question");
+const options = document.querySelector(".quiz-container .options");
+const nextBtn = document.querySelector(".quiz-container .next-btn");
+const quizResult = document.querySelector(".quiz-result");
+const startBtnContainer = document.querySelector(".start-btn-container");
+const startBtn = document.querySelector(".start-btn-container .start-btn");
+
+let questionNumber = 0;
+let score = 0;
+const MAX_QUESTIONS = 10;
+let timerInterval;
+
+const shuffleArray = (array) => {
+  return array.slice().sort(() => Math.random() - 0.5);
+};
+
+quizData = shuffleArray(quizData);
+
+const resetLocalStorage = () => {
+  for (i = 0; i < MAX_QUESTIONS; i++) {
+    localStorage.removeItem(`userAnswer_${i}`);
+  }
+};
+
+resetLocalStorage();
+
+const checkAnswer = (e) => {
+  let userAnswer = e.target.textContent;
+  if (userAnswer === quizData[questionNumber].correct) {
+    score++;
+    e.target.classList.add("correct");
+  } else {
+    e.target.classList.add("incorrect");
+  }
+
+  localStorage.setItem(`userAnswer_${questionNumber}`, userAnswer);
+
+  let allOptions = document.querySelectorAll(".quiz-container .option");
+  allOptions.forEach((o) => {
+    o.classList.add("disabled");
+  });
+};
+
+const createQuestion = () => {
+  clearInterval(timerInterval);
+
+  let secondsLeft = 9;
+  const timerDisplay = document.querySelector(".quiz-container .timer");
+  timerDisplay.classList.remove("danger");
+
+  timerDisplay.textContent = `Kalan Süre: 10 saniye`;
+
+  timerInterval = setInterval(() => {
+    timerDisplay.textContent = `Kalan Süre: ${secondsLeft
+      .toString()
+      .padStart(2, "0")} saniye`;
+    secondsLeft--;
+
+    if (secondsLeft < 3) {
+      timerDisplay.classList.add("danger");
+    }
+
+    if (secondsLeft < 0) {
+      clearInterval(timerInterval);
+      displayNextQuestion();
+    }
+  }, 1000);
+
+  options.innerHTML = "";
+  question.innerHTML = `<span class='question-number'>${
+    questionNumber + 1
+  }/${MAX_QUESTIONS}</span>${quizData[questionNumber].question}`;
+
+  const shuffledOptions = shuffleArray(quizData[questionNumber].options);
+
+  shuffledOptions.forEach((o) => {
+    const option = document.createElement("button");
+    option.classList.add("option");
+    option.innerHTML = o;
+    option.addEventListener("click", (e) => {
+      checkAnswer(e);
+    });
+    options.appendChild(option);
+  });
+};
+
+const retakeQuiz = () => {
+  questionNumber = 0;
+  score = 0;
+  quizData = shuffleArray(quizData);
+  resetLocalStorage();
+
+  createQuestion();
+  quizResult.style.display = "none";
+  quizContainer.style.display = "block";
+};
+
+const displayQuizResult = () => {
+  quizResult.style.display = "flex";
+  quizContainer.style.display = "none";
+  quizResult.innerHTML = "";
+
+  const resultHeading = document.createElement("h2");
+  resultHeading.innerHTML = `${MAX_QUESTIONS} Soruda ${score} Doğrun Var.`;
+  quizResult.appendChild(resultHeading);
+
+  for (let i = 0; i < MAX_QUESTIONS; i++) {
+    const resultItem = document.createElement("div");
+    resultItem.classList.add("question-container");
+
+    const userAnswer = localStorage.getItem(`userAnswer_${i}`);
+    const correctAnswer = quizData[i].correct;
+
+    let answeredCorrectly = userAnswer === correctAnswer;
+
+    if (!answeredCorrectly) {
+      resultItem.classList.add("incorrect");
+    }
+
+    resultItem.innerHTML = `<div class="Soru">Soru ${i + 1}: ${
+      quizData[i].question
+    }</div>
+    <div class="user-answer">Cevabın: ${userAnswer || "Cevap Verilmedi"}</div>
+    <div class="correct-answer">Doğru Cevap: ${correctAnswer}</div>`;
+
+    quizResult.appendChild(resultItem);
+  }
+
+  const retakeBtn = document.createElement("button");
+  retakeBtn.classList.add("retake-btn");
+  retakeBtn.innerHTML = "Tekrar Dene";
+  retakeBtn.addEventListener("click", retakeQuiz);
+  quizResult.appendChild(retakeBtn);
+};
+
+const displayNextQuestion = () => {
+  if (questionNumber >= MAX_QUESTIONS - 1) {
+    displayQuizResult();
+    return;
+  }
+
+  questionNumber++;
+  createQuestion();
+};
+
+nextBtn.addEventListener("click", displayNextQuestion);
+
+startBtn.addEventListener("click", () => {
+  startBtnContainer.style.display = "none";
+  quizContainer.style.display = "block";
+  createQuestion();
+});
